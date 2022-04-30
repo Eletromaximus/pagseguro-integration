2be0c232c3561745f779aff67bab01da53bfe83c
Max Milliano <maxmillianox@protonmail.com>
2022-04-30 19:44:16 -0300

    feat(primeiro commit):testando a api da pagseguro, ainda incompleta

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import 'express-async-errors'
 import helmet from 'helmet'
+import fetch from 'node-fetch'
+import 'dotenv'
 
 const app = express()
 
@@ -11,6 +13,24 @@ app.use(helmet())
 
 app.use(cors())
 
+async function teste () {
+  await fetch(
+    'https://sandbox.api.pagseguro.com/oauth2/application', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer' + process.env.APP_KEY
+      },
+      body: JSON.stringify({
+        name: 'MAXIMUS',
+        description: 'site de canecas estilosas'
+      })
+    }
+  )
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
+
 app.listen(process.env.PORT || 4000, () => {
-  console.log('Bem vindo ao server')
+  console.log('Bem vindo ao server'),
+  teste()
 })
