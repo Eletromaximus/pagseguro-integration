de28c2b8487018505975bf7ee6e3a5886f7e9c04
Max Milliano <maxmillianox@protonmail.com>
2022-05-05 21:56:25 -0300

    feat(crud concluido): criaço de modelos segue adiante

--- src/controllers/CartsController.ts
+++ src/controllers/CartsController.ts
@@ -28,4 +28,49 @@ export class CartsController {
       })
     }
   }
+
+  async update (req: Request, res: Response) {
+    try {
+      const { id } = req.params
+      const { code, price } = req.body
+
+      const cart = await Cart.findById(id)
+
+      if (!cart) {
+        return res.status(404).json()
+      }
+
+      await cart.updateOne({ code, price })
+
+      return res.status(200).json()
+    } catch (err) {
+      console.log(err)
+
+      return res.status(500).json({
+        error: 'Internal server error.'
+      })
+    }
+  }
+
+  async destroy (req: Request, res: Response) {
+    try {
+      const { id } = req.params
+
+      const cart = await Cart.findById(id)
+
+      if (!cart) {
+        return res.status(404).json()
+      }
+
+      await cart.deleteOne()
+
+      return res.status(200).json()
+    } catch (err) {
+      console.log(err)
+
+      return res.status(500).json({
+        error: 'Internal server error.'
+      })
+    }
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,30 @@
 import Router from 'express'
-import { UseCases } from './UseCases'
+// import { UseCases } from './UseCases'
 import { CartsController } from './controllers/CartsController'
 
-const cases = new UseCases()
+// const cases = new UseCases()
 const cartsController = new CartsController()
 
 const routes = Router()
 
-routes.post('/createApp', (req, res) => {
-  return cases.createApp(req, res)
-})
+// routes.post('/createApp', (req, res) => {
+//   return cases.createApp(req, res)
+// })
 
 routes.get('/carts', (req, res) => {
   return cartsController.index(req, res)
 })
 
-routes.get('/carts', (req, res) => {
+routes.post('/carts', (req, res) => {
   return cartsController.create(req, res)
+})
+
+routes.put('/carts/:id', (req, res) => {
+  return cartsController.update(req, res)
+})
+
+routes.delete('/carts/:id', (req, res) => {
+  return cartsController.destroy(req, res)
 })
 
 export { routes }
